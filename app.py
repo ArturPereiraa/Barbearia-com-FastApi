7480966aacc4124066811824832b9f03e9459ded
Artur Pereira <arturaul92@gmail.com>
2024-06-12 10:53:37 -0300

    commit inicial

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 # import uvicorn
 # from fastapi import FastAPI, Request
 # from fastapi.responses import HTMLResponse
@@ -134,10 +133,16 @@ def get_db():
     finally:
         db.close()
 
-# Rota GET para exibir o formulário de agendamento
+# Rota para pagina principal
 
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Rota para a página agendamento
+
+@app.get("/agendamento", response_class=HTMLResponse)  
+async def read_agendamento(request: Request):
     return templates.TemplateResponse("agendamento.html", {"request": request})
 
 # Rota POST para processar o agendamento
@@ -146,20 +151,16 @@ async def read_form(request: Request):
 async def agendar(nome: str = Form(...), data: str = Form(...), hora: str = Form(...), db: Session = Depends(get_db)):
     try:
         # Formata a data e a hora recebidas
-
         data_formatada = datetime.strptime(data, "%Y-%m-%d").date()
         hora_formatada = datetime.strptime(hora, "%H:%M").time()
     except ValueError:
-
         # Lança uma exceção HTTP se a data ou hora estiverem em um formato inválido
-
         raise HTTPException(status_code=400, detail="Formato de data ou hora inválido")
     
     # Cria um novo objeto de agendamento
     agendamento = Agendamento(nome=nome, data=data_formatada, hora=hora_formatada)
     
     # Adiciona o agendamento ao banco de dados e confirma a transação
-
     db.add(agendamento)
     db.commit()
     db.refresh(agendamento)
@@ -167,58 +168,21 @@ async def agendar(nome: str = Form(...), data: str = Form(...), hora: str = Form
     return {"message": "Agendamento realizado com sucesso!"}
 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Rota para a página principal
-
-=======
-import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
-from models.models import Base
-from crud.crud import get_users, create_user
-from database.connection import SessionLocal, engine  
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
 
-# Rota para a página principal
->>>>>>> b82a2abc6a6776dc757eb49add006919d9138603
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    users = get_users(SessionLocal())
-    return templates.TemplateResponse("index.html", {"request": request, "users": users})
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b82a2abc6a6776dc757eb49add006919d9138603
 # Rota para a página cortes
 
 @app.get("/cortes", response_class=HTMLResponse)  
 async def read_cortes(request: Request):
     return templates.TemplateResponse("cortes.html", {"request": request})
 
-<<<<<<< HEAD
 
-# Rota para a página agendamento
-@app.get("/agendamento", response_class=HTMLResponse)  
-async def read_agendamento(request: Request):
-    return templates.TemplateResponse("agendamento.html", {"request": request})
 
 
 
 # Executa o servidor Uvicorn
 if __name__ == "__main__":
     uvicorn.run(app)  
-=======
-uvicorn.run(app)
->>>>>>> b82a2abc6a6776dc757eb49add006919d9138603
